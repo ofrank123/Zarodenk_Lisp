5747d67d040773462ebe8fd45ca33bc8cf094f45
Jude Grodesky <jgrodesky@stuy.edu>
2018-06-01 09:17:54 -0400

    Token Costructor

--- src/common/Token.java
+++ src/common/Token.java
@@ -1,5 +1,7 @@
 package common;
 
+import java.util.regex.Pattern;
+    
 /***********************
  * public class Token
  * Token contains a number, symbol, String, or Parentheses
@@ -22,18 +24,56 @@ public class Token
     {
 	_data = data;
 	
-	if (data.equals("(")) {
+	if (Pattern.matches("(", data)) {
 	    _type = TokenType.LPAREN;
 	}
-	else if (data.equals(")")) {
+	else if (Pattern.matches(")", data)) {
 	    _type = TokenType.RPAREN;
 	}
-	else if ( data.substring(0, 1).equals('"') ) {
+	else if (Pattern.matches(" \"*\" " , data) ) {
 	    _type = TokenType.STR;
 	}
-
-
-
-
+	else if (Pattern.matches("/^[-+]?\\d+$/", data) ) {
+	    _type = TokenType.NUM;
+	}
+	else {
+	    _type = TokenType.SYM;
+	}
     }//end constructor
+
+    /***************
+     *Accessor for _type
+     *
+     *@author Jude Grodesky
+     ***************/
+    public TokenType getType(){
+	return _type;
+    }
+
+    /***************
+     *Accessor for _data
+     *
+     *@author Jude Grodesky
+     ***************/
+    public String getData(){
+	return _data;
+    }
+
+
+    /******************
+     *Main testing constructor
+     *
+     *@author Jude Grodesky
+     ******************/
+    public static void main(String[] args) {
+	Token a = new Token("(");
+	System.out.println(a.getType());
+
+	Token b = new Token(")");
+	System.out.println(b.getType());
+
+
+
+
+    }//end main
 }//end Token
