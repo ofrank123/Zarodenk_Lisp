ba97f05bf926835150a9b36b766883cbe750b6e7
th-ht-zaw <tzaw@stuy.edu>
2018-06-03 22:00:58 -0400

    TokenTester

---
+++ tests/TokenTester.java
@@ -0,0 +1,33 @@
+import common.*; 
+
+public class TokenTester
+{
+    public static void main(String[] args)
+    {
+
+	Token a = new Token("(");
+	System.out.println( a.getData() ); 
+	System.out.println( a.getType() );
+	
+	Token b = new Token(")");
+	System.out.println( "\n" + b.getData() );
+	System.out.println( b.getType() );
+	
+	Token c = new Token("def");
+	System.out.println( "\n" + c.getData() );
+	System.out.println( c.getType() );
+	
+	Token d = new Token("<");
+	System.out.println( "\n" + d.getData() );
+	System.out.println( d.getType() );
+
+	Token e = new Token("Big");
+	System.out.println("\n" + e.getData() );
+	System.out.println( e.getType() );
+
+	Token f = new Token("15");
+	System.out.println("\n" + f.getData() );
+	System.out.println( f.getType() );
+	
+    } 
+}
